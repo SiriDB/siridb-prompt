d78ef5ff2afe7c5da38baecff4a94ed6889b3e0f
Jeroen van der Heijden <jeroen@transceptor.technology>
2017-08-22 19:53:34 +0200

    Added import json and csv

--- query.go
+++ query.go
@@ -1,7 +1,12 @@
 package main
 
-import "encoding/json"
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 type query struct {
 	req string
@@ -18,8 +23,59 @@ func newQuery(req string) *query {
 	return &q
 }
 
+func readJSON(b []byte, v *interface{}) error {
+	reader := bytes.NewReader(b)
+	decoder := json.NewDecoder(reader)
+	decoder.UseNumber()
+	if err := decoder.Decode(v); err != nil {
+		return err
+	}
+	return nil
+}
+
+func readCSV(b []byte, v *interface{}) error {
+	reader := bytes.NewReader(b)
+	res, err := parseCsv(reader)
+	if err != nil {
+		return err
+	}
+
+	*v = res
+	return nil
+}
+
+func importFromFile(fn string, timeout uint16) (interface{}, error) {
+	var v interface{}
+	var loader func(b []byte, v *interface{}) error
+
+	if strings.HasSuffix(strings.ToLower(fn), ".json") {
+		loader = readJSON
+	} else if strings.HasSuffix(strings.ToLower(fn), ".csv") {
+		loader = readCSV
+	} else {
+		return nil, fmt.Errorf("Only .json or .csv files are supported")
+	}
+
+	data, err := ioutil.ReadFile(fn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = loader(data, &v)
+	if err != nil {
+		return nil, err
+	}
+
+	return client.Insert(v, timeout)
+}
+
 func (q *query) parse(timeout uint16) {
-	q.res, q.err = client.Query(q.req, timeout)
+	if strings.HasPrefix(q.req, "import ") {
+		fn := strings.TrimSpace(q.req[6:])
+		q.res, q.err = importFromFile(fn, timeout)
+	} else {
+		q.res, q.err = client.Query(q.req, timeout)
+	}
 }
 
 func (q *query) json() (string, error) {
