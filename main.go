a7f2b623b7cc40d4804c516c02be462199c5bc9f
Jeroen van der Heijden <jeroen@transceptor.technology>
2017-11-09 12:12:43 +0100

    Added logo and version

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 )
 
 // AppVersion exposes version information
-const AppVersion = "2.1.0"
+const AppVersion = "2.1.1"
 
 var (
 	xApp      = kingpin.New("siridb-admin", "Tool for communicating with a SiriDB database.")
